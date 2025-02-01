a7fae4d4efb4e66d648e85c325340a393d92e176
tecte <tecte1023@gmail.com>
2025-02-02 00:04:03 +0900

    1108. 검색 엔진

--- tecte/Baekjoon/BOJ_1108_검색엔진.java
+++ tecte/Baekjoon/BOJ_1108_검색엔진.java
@@ -1,7 +1,10 @@
 /*
- * 문 제 : 백준 1108번 - 검색 엔진 (https://www.acmicpc.net/problem/1108)
+ * 문제: 백준 1108번 - 검색 엔진 (https://www.acmicpc.net/problem/1108)
  * 문제 풀이
+ *  타잔 알고리즘을 사용하여 SCC(Strongly Connected Component, 강한 연결 요소)를 찾음.
+ *  위상 정렬로 다른 SCC로 향하는 링크만 점수를 더함.
  *
+ * 시간 복잡도: O(v + e) - v: 등장한 사이트의 개수, e: 링크의 개수
  */
 
 package Baekjoon;
@@ -9,115 +12,111 @@ package Baekjoon;
 import java.io.*;
 import java.util.*;
 
-// public class Main {
 public class BOJ_1108_검색엔진 {
-    static ArrayList<Integer>[] link = new ArrayList[1251];
-    static Stack<Integer> stack = new Stack<>();
-    static int[] scc;
-    static boolean[] checkScc;
+    // public class Main {
+    private static int id = 0;
+    private static Stack<Site> stack = new Stack<>();
+
+    private static int dfs(Site site) {
+        int parent = site.id = ++id;
+
+        stack.push(site);
+
+        for (Site next : site.link) {
+            if (next.id == 0) {
+                parent = Math.min(parent, dfs(next));
+            } else if (next.parent == 0) {
+                parent = Math.min(parent, next.id);
+            }
+        }
+
+        if (parent == site.id) {
+            Site child;
+
+            do {
+                child = stack.pop();
+                child.parent = parent;
+            } while (child.id != parent);
+        }
+
+        return parent;
+    }
+
+    private static class Site {
+        int id = 0;
+        int parent = 0;
+        long score = 1;
+        int inDegree = 0;
+        List<Site> link = new ArrayList<>();
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-        HashMap<String, Integer> site = new HashMap<>();
-        int[] rank;
-        int[] inDegree;
-        ArrayList<Integer>[] sccLink;
-        Queue<Integer> queue = new LinkedList<>();
-        int siteCnt = 0;
-        int website;
+        Map<String, Site> sites = new HashMap<>();
+        Queue<Site> queue = new LinkedList<>();
 
         for (int i = 0; i < n; i++) {
-            String[] input = br.readLine().split(" ");
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            Site site = sites.get(name);
+            int c = Integer.parseInt(st.nextToken());
 
-            if (site.get(input[0]) == null) {
-                site.put(input[0], ++siteCnt);
-                link[siteCnt] = new ArrayList<>();
+            if (site == null) {
+                site = new Site();
+                sites.put(name, site);
             }
 
-            for (int j = 2; j < input.length; j++) {
-                if (site.get(input[j]) == null) {
-                    site.put(input[j], ++siteCnt);
-                    link[siteCnt] = new ArrayList<>();
+            for (int j = 0; j < c; j++) {
+                String subName = st.nextToken();
+                Site subSite = sites.get(subName);
+
+                if (subSite == null) {
+                    subSite = new Site();
+                    sites.put(subName, subSite);
                 }
 
-                link[site.get(input[0])].add(site.get(input[j]));
+                subSite.link.add(site);
             }
         }
 
-        scc = new int[siteCnt + 1];
-        checkScc = new boolean[siteCnt + 1];
-        rank = new int[siteCnt + 1];
-        inDegree = new int[siteCnt + 1];
-        sccLink = new ArrayList[siteCnt + 1];
-
-        for (int i = 1; i <= siteCnt; i++) {
-            rank[i] = 1;
-            sccLink[i] = new ArrayList<>();
-
-            if (scc[i] == 0)
-                dfs(i);
+        for (Site site : sites.values()) {
+            if (site.parent == 0) {
+                dfs(site);
+            }
         }
 
-        for (int i = 1; i <= siteCnt; i++) {
-            for (int node : link[i]) {
-                if (scc[i] != scc[node]) {
-                    inDegree[i]++;
-                    sccLink[node].add(i);
+        for (Site site : sites.values()) {
+            for (Site next : site.link) {
+                if (site.parent != next.parent) {
+                    next.inDegree++;
                 }
             }
-
-            if (inDegree[i] == 0)
-                queue.offer(i);
         }
 
-        website = site.get(br.readLine());
+        for (Site site : sites.values()) {
+            if (site.inDegree == 0) {
+                queue.offer(site);
+            }
+        }
 
         while (!queue.isEmpty()) {
-            int node = queue.poll();
+            Site site = queue.poll();
 
-            if (node == website)
-                break;
+            for (Site next : site.link) {
+                if (site.parent != next.parent) {
+                    next.score += site.score;
 
-            for (int next : sccLink[node]) {
-                rank[next] += rank[node];
-                inDegree[next]--;
-
-                if (inDegree[next] == 0)
-                    queue.offer(next);
-            }
-        }
-
-        bw.write(Integer.toString(rank[website]));
-        bw.flush();
-        br.close();
-        bw.close();
-    }
-
-    public static int dfs(int v) {
-        if (scc[v] == 0) {
-            scc[v] = v;
-            stack.add(v);
-            checkScc[v] = true;
-
-            for (int next : link[v])
-                if (checkScc[next] || scc[next] == 0)
-                    scc[v] = Math.min(scc[v], dfs(next));
-
-            if (scc[v] == v) {
-                while (true) {
-                    int node = stack.pop();
-
-                    scc[node] = v;
-                    checkScc[node] = false;
-
-                    if(node == v)
-                        break;
+                    if (--next.inDegree == 0) {
+                        queue.offer(next);
+                    }
                 }
             }
         }
 
-        return scc[v];
+        bw.write(Long.toString(sites.get(br.readLine()).score));
+        br.close();
+        bw.close();
     }
 }
